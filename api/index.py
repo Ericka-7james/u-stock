02cacf34f2dd2fdb0b1d02c3ae1dec7869c9cd61
Ericka-7james <james7.ericka@gmail.com>
2025-12-13 02:07:05 -0500

    fix - fix Cors issue between bacn and front end

--- api/index.py
+++ api/index.py
@@ -32,7 +32,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
-    allow_origin_regex=CORS_ORIGIN_REGEX,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
